35b97dcaf4082e218463af5322ceb3c1e8e2a3a3
SeungukLee <seunguk@seunguk.dev>
2021-08-24 21:24:53 +0900

    add: base

---
+++ src/base.ts
@@ -0,0 +1,14 @@
+export type Contact = {
+  fistName: string;
+  middleInitital: string;
+  lastName: string;
+
+  emailAddress: string;
+  isEmailVerified: boolean;
+
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  isAddressValid: boolean;
+}
