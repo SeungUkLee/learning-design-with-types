edaa0e2e10883fd22bd902653146992699eb8e82
SeungukLee <seunguk@seunguk.dev>
2021-08-30 22:49:48 +0900

    add: step4

---
+++ src/step4/StateCode.ts
@@ -0,0 +1,34 @@
+import type { Branded, Option, Unwrap } from "../utils";
+import { some, none } from '../utils';
+
+const isStateCodes = (s: string): s is T => {
+  const sUpper = s.toUpperCase();
+  const stateCodes = ["AZ", "CA", "NY"];
+
+  return stateCodes.includes(sUpper);
+}
+
+export type T = Branded<string, 'StateCode'>;
+
+// create with continuation
+export const createWithCont = <A>(success: (a: T) => A) => (failure: (s: string) => A) => (s: string) => {
+  return isStateCodes(s) ? success(s) : failure("State is not in list.");
+}
+
+// create directly
+export const create = (s: string): Option<T> => {
+  const success = (e: T) => some(e)
+  const failure = (_: unknown) => none
+
+  return createWithCont(success)(failure)(s)
+}
+
+// unwrap with continuation
+export const apply = <A>(f: (s: string) => A) => (e: T) => f(e);
+
+// unwrap directly
+export const value = (e: T): Unwrap<T> => {
+  const id = <A>(a: A): A => a;
+  return apply(id)(e)
+}
+
