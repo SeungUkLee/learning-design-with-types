e301ef2ac2dfee239a41ce9a89ef8fa47e2e6d81
SeungukLee <seunguk@seunguk.dev>
2021-09-04 00:49:02 +0900

    edit: branded type

--- src/utils/Branded.ts
+++ src/utils/Branded.ts
@@ -3,6 +3,12 @@
 // type Tagged<Tag extends string> = { [_brand]: Tag }
 type Tagged<Tag extends string> = { _tag: Tag }
 
-export type Branded<Type, Tag extends string> = Type & Tagged<Tag>
+// TODO:
+export type Branded<Type, Tag extends string> = (
+  Type extends { _tag: any }
+    ? { data: Type }
+    : Type
+) & Tagged<Tag>
+
 export type Unwrap<T> = T extends Branded<infer Type, string> ? Type : never;
 
